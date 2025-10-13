9e81c649fae5fc18191af70cb196339015117dd2
sca <yao.fjk@obmontessori.edu.ph>
2025-10-13 20:51:41 +0800

    main.py

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
-from pyscript import display
+from pyscript import display, document
 
 
 restaurant_name = "Baker Bear Bakery" # string
 owner_name = "Francesca Yao" # string
-year_established = 2009 #float
+year_established = 2009 #integer
 popular_item_price = 149.50 # float
 has_delivery = True # boolean
 product_names = ['Strawberry Bunny','Lemon Duck','Blueberry Seal','Pistachio Parakeet','Cookies n Panda Dream', 'Choco Bear'] # list
-business_hours = 'Open 7:00am - 10:00pm'
-menu_prices = ['$20.00','$15.00','$20.00','$15.00','$20.00','$20.00'] # list
+menu_prices = ['20.00','15.00','20.00','15.00','20.00','20.00'] # list
 common_allergens = ['eggs','milk','wheat']
-tax_rate = 0.05 # floa
+business_hours = 'Open 7:00am - 10:00pm'
+tax_rate = 0.05 # float
+
+display(f"{product_names[0]} - ${menu_prices[0]}", target='menu_list')
+display(f"{product_names[1]} - ${menu_prices[1]}", target='menu_list')
+display(f"{product_names[2]} - ${menu_prices[2]}", target='menu_list')
+display(f"{product_names[3]} - ${menu_prices[3]}", target='menu_list')
+display(f"{product_names[4]} - ${menu_prices[4]}", target='menu_list')
+display(f"{product_names[5]} - ${menu_prices[5]}", target='menu_list')
 
 
-display(f"A cozy welcome to {restaurant_name}!", 
-target='div1')
+# ORDER SUMMARY/CUSTOMER DETAILS
 
-display(f"serving you since {year_established}", 
-target='div2')
+def place_order(e):
+    c_name = document.getElementById('cname').value
+    c_address = document.getElementById('address').value
+    c_number = document.getElementById('cnumber').value
 
-display(f"Our Menu {product_names}{menu_prices}",
-target='div4')
+    customer_details = f'''
+        Student Profile
+        Order for: {c_name}
+        Address: {c_address}
+        Contact number: {c_number}
+        '''
+    display(customer_details, target='summary')
